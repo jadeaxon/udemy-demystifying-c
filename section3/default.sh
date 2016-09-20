653dfe7350ea826e1055e12694658dfcab374f3c
Jeff Anderson <jeff.anderson@digecor.com>
2016-09-20 09:05:33 -0700

    Update default.sh

--- section3/default.sh
+++ section3/default.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-last_modded=$(ls -1t *.c | head -1)
+set -o pipefail
+
+# This script is triggered by . alias which calls do_default_action(). 
+# This mechanism is defined in by .bashrc via my bash-glory lib.
+
+# Compile and run the most recently modified .c file.
+last_modded=$(ls -1t *.c 2> /dev/null | head -1)
+if [ -z "$last_modded" ]; then
+	ls -lahF --color
+	exit 0
+fi
+
 if gcc $last_modded |& tee make.out; then
 	./a.out
 else
