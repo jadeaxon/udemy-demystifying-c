c9051cb4911c7e00fcbc06e18eeb262e18ee51f8
Jeff Anderson <jeff.anderson@digecor.com>
2016-09-24 13:17:53 -0600

    Finish exercise 3

---
+++ section4/3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// 3. Write a program to print factorial of any positive integer entered by the user through the keyboard.
+
+// Calculates factorial.  Works for values 0 to 33.
+// It appears that 34 overflows to the value 0 causing everything beyond it to have value 0.
+// It actually overflows well before that but does not hit 0.
+unsigned int factorial(unsigned int n) {
+	if (n == 0) return 1; // Base case.
+	return n * factorial(n - 1); // Recursion.
+}
+
+// Looks like max this will take you to is 40 factorial.
+// You'd need some kind of bigint lib to go further.
+unsigned long long big_factorial(unsigned int n) {
+	if (n == 0) return 1; // Base case.
+	return n * big_factorial(n - 1); // Recursion.
+}
+
+int main(void) {
+	unsigned int number = 0;
+	printf("number: ");
+	scanf("%u", &number);
+
+	unsigned int f = factorial(number);
+	printf("f(%u) = %u\n", number, f);
+
+	unsigned long long f2 = big_factorial(number);
+	printf("f(%u) = %llu\n", number, f2);
+
+} // main()
+
+
